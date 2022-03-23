1ffaca229b6699d7dec792b7482c4abc8b6f7838
hayj04 <hayj04@gmail.com>
2022-03-23 19:03:30 +0900

    [S06P22A403-17] [BE] ✨feat: MongoRepository 사용을 위한 DefenseRepository 정의

---
+++ SeoulEmergency/src/main/java/com/SeoulEmergency/core/repository/DefenseRepository.java
@@ -0,0 +1,8 @@
+package com.SeoulEmergency.core.repository;
+
+import com.SeoulEmergency.core.domain.shelter.Defense;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DefenseRepository extends CrudRepository<Defense, String> {
+
+}
