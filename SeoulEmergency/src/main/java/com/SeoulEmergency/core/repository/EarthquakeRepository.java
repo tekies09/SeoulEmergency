1cc6871b288186feb133fe5f77847f241298e059
hayj04 <hayj04@gmail.com>
2022-03-25 01:51:46 +0900

    [S06P22A403-17] [BE] 🔥remove: MongoRepository에서 MongoTemplate 구조에 맞게 변경하기 위해 EarthquakeRepository 제거

--- SeoulEmergency/src/main/java/com/SeoulEmergency/core/repository/EarthquakeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.SeoulEmergency.core.repository;
-
-import com.SeoulEmergency.core.domain.shelter.Earthquake;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EarthquakeRepository extends CrudRepository<Earthquake, String> {
-}
