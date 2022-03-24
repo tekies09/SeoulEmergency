36ddf5d2d2152463dbeee7e78e67a4dfe87dc701
hayj04 <hayj04@gmail.com>
2022-03-25 01:47:16 +0900

    [S06P22A403-17] [BE] ✨feat: MongoRepository에서 MongoTemplate 구조로 변경하기 위해 MongoConfig 설정

---
+++ SeoulEmergency/src/main/java/com/SeoulEmergency/config/MongoConfig.java
@@ -0,0 +1,29 @@
+package com.SeoulEmergency.config;
+
+import com.mongodb.MongoClientURI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+@Configuration
+public class MongoConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public MongoDbFactory mongoDbFactory() {
+        return new SimpleMongoDbFactory(new MongoClientURI(env.getProperty("spring.data.mongodb.uri")));
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
+        return mongoTemplate;
+    }
+
+}
