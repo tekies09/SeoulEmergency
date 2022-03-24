67df81fd9fdca73180092b857c4573ebd64f5c74
hayj04 <hayj04@gmail.com>
2022-03-25 01:44:36 +0900

    [S06P22A403-17] [BE] 🎨style: EarthquakeDetailRes 내부 Entity 타입 Backend 브랜치에 맞게 변경

--- SeoulEmergency/src/main/java/com/SeoulEmergency/api/dto/response/EarthquakeDetailRes.java
+++ SeoulEmergency/src/main/java/com/SeoulEmergency/api/dto/response/EarthquakeDetailRes.java
@@ -1,7 +1,6 @@
 package com.SeoulEmergency.api.dto.response;
 
-import com.SeoulEmergency.core.domain.shelter.Defense;
-import com.SeoulEmergency.core.domain.shelter.Earthquake;
+import com.SeoulEmergency.core.domain.EarthquakeShelter;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
@@ -17,9 +16,9 @@ import lombok.Setter;
 public class EarthquakeDetailRes extends BaseResponseBody {
 
     @ApiModelProperty(name = "지진옥외 대피소 객체")
-    Earthquake earthquakeDetail = new Earthquake();
+    EarthquakeShelter earthquakeDetail = new EarthquakeShelter();
 
-    public static EarthquakeDetailRes of(Integer statusCode, String message, Earthquake earthquakeDetail) {
+    public static EarthquakeDetailRes of(Integer statusCode, String message, EarthquakeShelter earthquakeDetail) {
         EarthquakeDetailRes res = new EarthquakeDetailRes();
         res.setStatusCode(statusCode);
         res.setMessage(message);
