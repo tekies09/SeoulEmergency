5ec6fcaab0247d325796fbc12f9cbb81abe0e2a0
hayj04 <hayj04@gmail.com>
2022-03-25 01:43:24 +0900

    [S06P22A403-17] [BE] 🎨style: ShelterService Backend 브랜치와 병합

--- SeoulEmergency/src/main/java/com/SeoulEmergency/api/service/ShelterService.java
+++ SeoulEmergency/src/main/java/com/SeoulEmergency/api/service/ShelterService.java
@@ -1,13 +1,17 @@
 package com.SeoulEmergency.api.service;
 
-import com.SeoulEmergency.core.domain.shelter.Defense;
-import com.SeoulEmergency.core.domain.shelter.Earthquake;
+import com.SeoulEmergency.core.domain.DefenseShelter;
+import com.SeoulEmergency.core.domain.EarthquakeShelter;
+
+import java.util.List;
 
 /**
  * 대피소 관련 비즈니스 로직을 처리하는 서비스 인터페이스
  */
 public interface ShelterService {
 
-    Earthquake getEarthquakeDetail(String shelterId);
-    Defense getDefenseDetail(String shelterId);
+    EarthquakeShelter getEarthquakeDetail(String shelterId);
+    DefenseShelter getDefenseDetail(String shelterId);
+    List<EarthquakeShelter> getNearEarthquakeShelters(String longitude, String latitude);
+    List<DefenseShelter> getNearDefenseShelters(String longitude, String latitude);
 }
