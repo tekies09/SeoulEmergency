39119f6c0e78ab9ac8461e1f9ac0304015d04b6d
이정환 <ready2start@naver.com>
2022-03-25 13:37:20 +0900

    [BE] :fire: chore: 중복되는 코드 삭제

--- SeoulEmergency/src/main/java/com/SeoulEmergency/api/controller/ShelterController.java
+++ SeoulEmergency/src/main/java/com/SeoulEmergency/api/controller/ShelterController.java
@@ -14,6 +14,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
