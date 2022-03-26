f17233e0e3e9892ca814fb19a81b1a56a8c82d3b
hayj04 <hayj04@gmail.com>
2022-03-27 01:54:24 +0900

    [S06P22A403-15] [FE] ✨feat: 대피소 검색 기능 구현

--- seoul-emergency-front/src/components/SearchBar.vue
+++ seoul-emergency-front/src/components/SearchBar.vue
@@ -3,9 +3,14 @@
       <b-input-group>
         <b-form-select v-model="$store.state.category" :options="$store.state.options" @change="onChange($store.state.category)"></b-form-select>
 
-        <b-form-input type="search"></b-form-input>
+        <b-form-input 
+          type="search"
+          v-model="searchInput">
+        </b-form-input>
 
-        <b-button variant="light">
+        <b-button 
+          variant="light"
+          @click="searchClicked">
           <img src="../assets/search.svg" alt="Search Icon" class="search-icon">
         </b-button>
       </b-input-group>
@@ -14,9 +19,63 @@
 
 <script>
 export default {
+  data() {
+    return {
+      searchInput: '',
+      categoryInput: null,
+    }
+  },
   methods: {
     onChange(categoryInput) {
       this.$store.state.category = categoryInput;
+    },
+    // 검색 버튼 클릭
+    searchClicked() {
+      var categoryInput = this.$store.state.category
+      // 카테고리 선택에 따른 API 호출
+      if (categoryInput == "지진") {
+        this.searchEarthquakes()
+        .then((res) => {
+          // API 호출 결과로 얻은 대피소 리스트를 store에 반영
+          this.$store.commit('setSearchShelterList', res.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      } else if (categoryInput == "해일") {
+        this.searchTsunamis()
+        .then((res) => {
+          // API 호출 결과로 얻은 대피소 리스트를 store에 반영
+          this.$store.commit('setSearchShelterList', res.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      } else if (categoryInput == "민방위") {
+        this.searchDefenses()
+        .then((res) => {
+          // API 호출 결과로 얻은 대피소 리스트를 store에 반영
+          this.$store.commit('setSearchShelterList', res.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      }
+    },
+    // 지진 대피소 리스트 검색 API 비동기 호출
+    async searchEarthquakes() {
+      // return await this.$store.dispatch('searchEarthquakesByName', this.searchInput)            // 이름 검색
+      return await this.$store.dispatch('searchEarthquakesByArea', this.searchInput)               // 지명 검색
+    },
+    // 해일 대피소 리스트 검색 API 비동기 호출
+    async searchTsunamis() {
+      // return await this.$store.dispatch('searchTsunamisByName', this.searchInput)               // 이름 검색
+      return await this.$store.dispatch('searchTsunamisByArea', this.searchInput)                  // 지명 검색
+    },
+    // 민방위 대피소 리스트 검색 API 비동기 호출
+    async searchDefenses() {
+      // return await this.$store.dispatch('searchDefensesByName', this.searchInput)               // 이름 검색
+      return await this.$store.dispatch('searchDefensesByArea', this.searchInput)                  // 지명 검색
     }
   }
 }
