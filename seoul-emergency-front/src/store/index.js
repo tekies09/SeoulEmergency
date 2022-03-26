a29545120c2c7db56177ff82974865204bff03cf
hayj04 <hayj04@gmail.com>
2022-03-27 01:55:36 +0900

    [S06P22A403-15] [FE] ✨feat: 대피소 리스트 store에 저장

--- seoul-emergency-front/src/store/index.js
+++ seoul-emergency-front/src/store/index.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
+
+axios.defaults.headers['Content-Type'] = 'application/json'
+const backAxios = axios.create({
+  baseURL: '/api'
+})
 
 export default new Vuex.Store({
   state: {
@@ -11,13 +17,59 @@ export default new Vuex.Store({
       { value: "지진", text: "지진"},
       { value: "해일", text: "해일"},
       { value: "민방위", text: "민방위"}
-    ]
+    ],
+    searchShelterList: [],      // 검색 API로 얻은 대피소 리스트
   },
   getters: {
+    // 대피소 리스트 getter
+    getSearchShelterList(state) {
+      return state.searchShelterList
+    }
   },
   mutations: {
+    // 대피소 리스트 setter
+    setSearchShelterList(state, searchShelterList) {
+      state.searchShelterList = []
+      state.searchShelterList = searchShelterList
+    }
   },
   actions: {
+    // 지진 옥외 대피소 지역명 검색 axios
+    searchEarthquakesByArea({state}, payload) {
+      console.log('searchEarthquakesByArea', state, payload);
+      const url = `/api/shelters/areasearch/earthquakes/${payload}`
+      return backAxios.get(url)
+    },
+    // 지진 옥외 대피소 이름 검색 axios
+    searchEarthquakesByName({state}, payload) {
+      console.log('searchEarthquakesByName', state, payload);
+      const url = `/api/shelters/namesearch/earthquakes/${payload}`
+      return backAxios.get(url)
+    },
+    // 해일 대피소 지역명 검색 axios
+    searchTsunamisByArea({state}, payload) {
+      console.log('searchTsunamisByArea', state, payload);
+      const url = `/api/shelters/areasearch/tsunamis/${payload}`
+      return backAxios.get(url)
+    },
+    // 해일 대피소 이름 검색 axios
+    searchTsunamisByName({state}, payload) {
+      console.log('searchTsunamisByName', state, payload);
+      const url = `/api/shelters/namesearch/tsunamis/${payload}`
+      return backAxios.get(url)
+    },
+    // 민방위 대피소 지역명 검색 axios
+    searchDefensesByArea({state}, payload) {
+      console.log('searchDefensesByArea', state, payload);
+      const url = `/api/shelters/areasearch/defenses/${payload}`
+      return backAxios.get(url)
+    },
+    // 민방위 대피소 이름 검색 axios
+    searchDefensesByName({state}, payload) {
+      console.log('searchDefensesByName', state, payload);
+      const url = `/api/shelters/namesearch/defenses/${payload}`
+      return backAxios.get(url)
+    }
   },
   modules: {
   }
