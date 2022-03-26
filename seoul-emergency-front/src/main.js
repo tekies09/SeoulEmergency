5bdba6e446d313f39b0b3678af53ee50007cb20b
hayj04 <hayj04@gmail.com>
2022-03-27 01:53:26 +0900

    [S06P22A403-15] [FE] ✨feat: 추가한 라이브러리 사용 설정

--- seoul-emergency-front/src/main.js
+++ seoul-emergency-front/src/main.js
@@ -7,10 +7,23 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Import Naver Map Vue Library
+import naver from 'vue-naver-maps'
+
+// Import VueGeolocationApi for Get Current Location
+import VueGeolocationApi from 'vue-geolocation-api'
+
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
+// Make VueNaverMap available by Set ClientId
+Vue.use(naver, {
+  clientID: 'nblzs8lwby',
+})
+// Make VueGeolocationApi available
+Vue.use(VueGeolocationApi)
 
 Vue.config.productionTip = false
 
