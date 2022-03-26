64953ae02f6f841071287e7d2d7959e52af5ee1c
hayj04 <hayj04@gmail.com>
2022-03-27 01:53:00 +0900

    [S06P22A403-15] [FE] ✨feat: 백엔드 서버 연결 설정

--- seoul-emergency-front/vue.config.js
+++ seoul-emergency-front/vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 8080,
+    open: true,
+    proxy: {
+      '/api' : {
+        target: 'http://j6a403.p.ssafy.io',
+        changeOrigin: true,
+      }
+    },
+  }
 })
