06362840b3f7edb4cd8117dd1dd83406d0dcac00
여율 <dbfm2222@gmail.com>
2022-03-30 14:47:05 +0900

    :sparkles: [FE] feat: 뉴스 리스트 페이지 1차 완성

--- seoul-emergency-front/src/components/WordCloud.vue
+++ seoul-emergency-front/src/components/WordCloud.vue
@@ -2,13 +2,13 @@
   <div class="wordcloud">
       <b-tabs>
         <b-tab title="단어로 보기">
-            <img src="https://via.placeholder.com/400x500?text=Word+Count">
+            <img src="https://via.placeholder.com/400x300?text=Word+Count">
         </b-tab>
         <b-tab title="차트로 보기">
-            <img src="https://via.placeholder.com/400x500?text=Chart">
+            <img src="https://via.placeholder.com/400x300?text=Chart">
         </b-tab>
         <b-tab title="머시기로 보기">
-            <img src="https://via.placeholder.com/400x500?text=Mosigi">
+            <img src="https://via.placeholder.com/400x300?text=Mosigi">
         </b-tab>
     </b-tabs>
   </div>
